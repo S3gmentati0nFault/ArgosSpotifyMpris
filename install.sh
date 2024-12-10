a0df49f971c6c32116a40697f94a8d8e2e6fc025
heartofgold <alessandro.biagiotti@studenti.unimi.it>
2024-12-10 10:39:41 +0100

    Modified the installation script to use a virtual environment, added the necessary steps to make sure that the requirements for things like Cairo were installed, moved from symlink because for me it did not work

--- install.sh
+++ install.sh
@@ -1,10 +1,31 @@
-#!/usr/bin/env bash
+#!/usr/bin/env fish
 set -e
 
-echo "[Creating symlink] spotify.r.1s+.py -> ~/.config/argos/spotify.r.1s+.py"
-ln -s spotify.r.1s+.py ~/.config/argos/spotify.r.1s+.py
+# Install dependencies for pycairo
+sudo apt-get install libcairo2-dev libjpeg-dev libgif-dev
+sudo apt-get install libgirepository1.0-dev
 
+# Check if there is a venv folder in the root folder of the project
+if not test -d "venv"
+	echo "Creating the virtual environment..."
+	python -m venv ./venv
+end
+
+# Check if pip-tools is installed
+set PIP_TOOLS (pip list | grep pip-tools)
+if test -z "$PIP_TOOLS"
+	echo "Installing pip-tools..."
+	pip install pip-tools
+end
+
+# Create the symlink
+echo "[Hard-linking the file] spotify.r.1s+.py -> ~/.config/argos/spotify.r.1s+.py"
+ln spotify.r.1s+.py ~/.config/argos/spotify.r.1s+.py
+
+# Install the requirements using the virtual environment
+source ./venv/bin/activate.fish
 echo "[Installing requirements]"
-pip3 install -r requirements.txt
+pip-compile requirements.in
+pip install -r requirements.txt
 
 echo "Extension installed!"
